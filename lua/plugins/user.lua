6d2b98e9045e501f280009f43c0dc125b205922e
Marc Ende <marc.ende_ext@external.mail.schwarz>
2024-12-29 07:58:17 +0100

    added zellij

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -4,7 +4,7 @@
 ---@type LazySpec
 return {
 
-  { "mrjones2014/smart-splits.nvim", lazy = false },
+  --  { "mrjones2014/smart-splits.nvim", lazy = false },
   --  {
   --    "shellRaining/hlchunk.nvim",
   --    event = { "BufReadPre", "BufNewFile" },
@@ -19,4 +19,16 @@ return {
   --      }
   --    end,
   --  },
+  {
+    "https://git.sr.ht/~swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+    },
+    opts = {},
+  },
 }
