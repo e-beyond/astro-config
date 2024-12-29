4b18dd34db83e1360e4e402a1bf7dc190db633f6
Marc Ende <marc.ende_ext@external.mail.schwarz>
2024-12-29 09:33:28 +0100

    modified

--- lua/plugins/user-copilot.lua
+++ lua/plugins/user-copilot.lua
@@ -5,7 +5,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
